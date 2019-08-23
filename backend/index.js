1d755b32424b0717dcfa8adc1a9380daa6eac2f0
Ben Waller <benwalleriv@gmail.com>
2019-08-23 08:04:59 -0700

    updates to models and mongodb connected

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,87 @@
-import express from "express";
-import bodyParser from "body-parser";
-import { graphqlExpress } from "graphql-server-express";
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
 
 const app = express();
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphqlSchema }));
+const events = [];
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`server listening on port ${port}...`);
-});
+app.use(bodyParser.json());
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
+      type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
+      input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
+      type RootQuery {
+        events: [Event!]!
+      }
+
+      type RootMutation {
+        createEvent(eventInput: EventInput): Event
+      }
+
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvent: (args) => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date()
+        });
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            return {...result._doc}
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+      }
+    },
+    graphiql: true
+  }),
+);
+
+//    CONN/LISTEN
+const port = process.env.PORT || 4000;
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@graphql-demo-dxeua.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`server listening on port ${port}...`);
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
